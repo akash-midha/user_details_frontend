948b7992bdfd804e226a72f2add8bd33b8221fac
Ayushiiism <ayushtripathy08@gmail.com>
2023-10-25 11:39:18 +0530

    modification

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,58 +5,63 @@ import axios from "axios";
 import { MdScience } from "react-icons/md";
 import { AiFillMobile, AiOutlineWifi } from "react-icons/ai";
 import { BiSolidLayer, BiSolidPhoneCall } from "react-icons/bi";
-import { BsFillPuzzleFill, BsLaptop } from "react-icons/bs";
+import { BsFillPuzzleFill, BsLaptop, BsBoxArrowInUpRight, BsThreeDotsVertical } from "react-icons/bs";
+import { RiIncreaseDecreaseFill } from "react-icons/ri";
 
 function Home() {
-  function clickHandler() {
-    alert("Div clicked!");
+
+
+  function clickHandler(val) {
+    setShow(val);
   }
 
-  const [users, setUsers] = useState([]);
-    const fetchInfo =async() => { 
-        try{
-            await axios.get('http://localhost:8080/api/user/allUser').then((res) =>{
-                console.log("Ayush",res.data)
-                fetchInfoByID(res.data[0].id)
-                setUsers(res.data);
-            });
-        }catch{
-            window.alert("Error 404 page not found")
-            console.log();
-        }
+
+  const [show, setShow] = useState(0)
+  const [users, setUsers] = useState([])
+  const fetchInfo = async () => {
+    try {
+      await axios.get('http://localhost:8080/api/user/allUser').then((res) => {
+        // console.log("Ayush",res.data)
+        fetchInfoByID(res.data[0].id)
+        setUsers(res.data);
+      });
+    } catch {
+      window.alert("Error 404 page not found")
+      console.log();
     }
-    const [userById,setUserById] = useState({
-      name : "",
-      device : "",
-      phone : "",
-      pid : "",
-      plan : {
-        name : "",
-        price : ""
-      }
-    });
-    const fetchInfoByID =async(value) => {
-      try{
-          await axios.get(`http://localhost:8080/api/user/userById/${value}`).then((res) =>{
-              console.log("AyushbyId",res.data)
-              setUserById(res.data);
-          });
-      }catch{
-          window.alert("Error 404 page not found")
-          console.log();
-      }
   }
-    useEffect(() => {
-      fetchInfo(); 
-}, [])
+  const [userById, setUserById] = useState({
+    name: "",
+    device: "",
+    phone: "",
+    pid: "",
+    plan: {
+      name: "",
+      price: ""
+    }
+  });
+  const fetchInfoByID = async (value) => {
+    try {
+      await axios.get(`http://localhost:8080/api/user/userById/${value}`).then((res) => {
+        // console.log("AyushbyId",res.data)
+        setUserById(res.data);
+      });
+    } catch {
+      window.alert("Error 404 page not found")
+      console.log();
+    }
+  }
+  useEffect(() => {
+    fetchInfo();
+  }, [])
 
-var currId;
-const handle = (e) =>{
- console.log(e.target.value)
- currId = e.target.value;
- fetchInfoByID(e.target.value);
+  var currId;
+  const handle = (e) => {
+    console.log(e.target.value)
+    currId = e.target.value;
+    fetchInfoByID(e.target.value);
 
-}
+  }
 
   console.log(users);
   return (
@@ -69,37 +74,37 @@ const handle = (e) =>{
               <MdScience />
             </div>
             <div className="justicon">
-              <MdScience />
+              <RiIncreaseDecreaseFill />
             </div>
             <div className="justicon">
-              <MdScience />
+              <BsBoxArrowInUpRight />
             </div>
           </div>
         </div>
         <div className="tabs">
           <div className="p-sview-icons-mobile-selected">
-            <div className="icon-circle" onClick={clickHandler}>
+            <div className="icon-circle" onClick={(e)=>clickHandler(0)}>
               <div className="iconsize">
                 <AiFillMobile />
               </div>
             </div>
           </div>
           <div className="p-sview-icons-mobile-selected">
-            <div className="icon-circle" onClick={clickHandler}>
+            <div className="icon-circle" onClick={(e)=>clickHandler(1)}>
               <div className="iconsize">
                 <AiOutlineWifi />
               </div>
             </div>
           </div>
           <div className="p-sview-icons-mobile-selected">
-            <div className="icon-circle" onClick={clickHandler}>
+            <div className="icon-circle" onClick={(e)=>clickHandler(2)}>
               <div className="iconsize">
                 <BiSolidPhoneCall />
               </div>
             </div>
           </div>
           <div className="p-sview-icons-mobile-selected">
-            <div className="icon-circle" onClick={clickHandler}>
+            <div className="icon-circle" onClick={(e)=>clickHandler(3)}>
               <div className="iconsize">
                 <BsLaptop />
               </div>
@@ -108,58 +113,66 @@ const handle = (e) =>{
         </div>
         <div className="rectangle"></div>
 
-        <select className="subscription-dropdown" onChange={handle}>
-          {
-                users.map((curUser,index)=>{
-                 return <option key={index} value={curUser.id}>{curUser.name}</option>
+        {show==0 &&
+        <div className="container">
+          <select className="subscription-dropdown" onChange={handle}>
+            {
+              users.length > 0 && users.map((curUser, index) => {
+                return <option key={index} value={curUser.id}>{<AiFillMobile /> }{curUser.phone} | {curUser.name}</option>
               })
-            
-          }
-        </select>
 
-        <div className="price">
-          <div className="firstDiv">
-            <div className="indicators-tags-default">Active</div>
-            <div className="thisprice">${userById.plan.price}</div>
+            }
+          </select>
+          <div className="price">
+            <div className="firstDiv">
+              <div className="indicators-tags-default">Active</div>
+              <div className="thisprice">${userById.plan.price}</div>
+            </div>
+            <div className="secondDiv">
+              <icon>
+                <BsThreeDotsVertical />
+              </icon>
+              <icon>
+                <BsBoxArrowInUpRight />
+              </icon>
+            </div>
           </div>
-          <div className="secondDiv">
+          <div className="group">
             <icon>
               <BiSolidLayer />
             </icon>
+            <span className="grp-des">{userById.plan.name}</span>
+            <span className="grp-action" onClick={clickHandler}>
+              <u>Change</u>
+            </span>
+          </div>
+          <div className="group">
             <icon>
-              <BiSolidLayer />
+              <AiFillMobile />
             </icon>
+            <span className="grp-des">{userById.device}</span>
+            <span className="grp-action" onClick={clickHandler}>
+              <u>Upgrade</u>
+            </span>
+          </div>
+          <div className="group">
+            <icon>
+              <BsFillPuzzleFill />
+            </icon>
+            <span className="grp-des">Add-ons(5)</span>
+            <span className="grp-action" onClick={clickHandler}>
+              <u>Manage</u>
+            </span>
           </div>
         </div>
-        <div className="group">
-          <icon>
-            <BiSolidLayer />
-          </icon>
-          <span className="grp-des">{userById.plan.name}</span>
-          <span className="grp-action" onClick={clickHandler}>
-            Change
-          </span>
-        </div>
-        <div className="group">
-          <icon>
-            <AiFillMobile />
-          </icon>
-          <span className="grp-des">{userById.device}</span>
-          <span className="grp-action" onClick={clickHandler}>
-            Change
-          </span>
-        </div>
-        <div className="group">
-          <icon>
-            <BsFillPuzzleFill />
-          </icon>
-          <span className="grp-des">Add-ons(5)</span>
-          <span className="grp-action" onClick={clickHandler}>
-            Change
-          </span>
-        </div>
+      }
+      {show==1 && <h3 className="diiii">Coming soon!</h3>}
+      {show==2 && <h3 className="diiii">Coming soon!</h3>}
+      {show==3 && <h3 className="diiii">Coming soon!</h3>}
       </div>
+          
     </div>
+          
   );
 }
 
